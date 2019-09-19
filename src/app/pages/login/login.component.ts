e0187f7dccf04fd21386b1dcb5610d4606f41ab1
Daniel Jiménez <geisondanieljl@gmail.com>
2019-09-18 23:52:10 -0500

    advance

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,24 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+  public error = null;
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
   }
   ngOnDestroy() {
+  }
+
+  onSubmit(form: NgForm) {
+    this.http.post('http://localhost:8000/api/login', form.value).subscribe(
+      data => console.log(data),
+      error => this.handleError(error)
+    );
+  }
+
+  handleError(error) {
+    this.error = error.error.error;
   }
 
 }
