3a54c5b4fcc708e7c0da8e9974d248d1fa81d74e
Daniel Jiménez <geisondanieljl@gmail.com>
2019-09-26 20:31:50 -0500

    fix

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -26,31 +26,31 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
 
-    this.datasets = [
-      [0, 20, 10, 30, 15, 40, 20, 60, 60],
-      [0, 20, 5, 25, 10, 30, 15, 40, 40]
-    ];
-    this.data = this.datasets[0];
+    // this.datasets = [
+    //   [0, 20, 10, 30, 15, 40, 20, 60, 60],
+    //   [0, 20, 5, 25, 10, 30, 15, 40, 40]
+    // ];
+    // this.data = this.datasets[0];
 
 
-    var chartOrders = document.getElementById('chart-orders');
+    // var chartOrders = document.getElementById('chart-orders');
 
-    parseOptions(Chart, chartOptions());
+    // parseOptions(Chart, chartOptions());
 
 
-    var ordersChart = new Chart(chartOrders, {
-      type: 'bar',
-      options: chartExample2.options,
-      data: chartExample2.data
-    });
+    // var ordersChart = new Chart(chartOrders, {
+    //   type: 'bar',
+    //   options: chartExample2.options,
+    //   data: chartExample2.data
+    // });
 
-    var chartSales = document.getElementById('chart-sales');
+    // var chartSales = document.getElementById('chart-sales');
 
-    this.salesChart = new Chart(chartSales, {
-			type: 'line',
-			options: chartExample1.options,
-			data: chartExample1.data
-		});
+    // this.salesChart = new Chart(chartSales, {
+		// 	type: 'line',
+		// 	options: chartExample1.options,
+		// 	data: chartExample1.data
+		// });
   }
 
 
@@ -58,8 +58,8 @@ export class DashboardComponent implements OnInit {
 
 
   public updateOptions() {
-    this.salesChart.data.datasets[0].data = this.data;
-    this.salesChart.update();
+    // this.salesChart.data.datasets[0].data = this.data;
+    // this.salesChart.update();
   }
 
 }
